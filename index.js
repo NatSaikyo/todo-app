31a519e8edc9e715b416f1874dccf0b4d2a85dd1
Natsuki Saikyo <natsukisaikyo@gmail.com>
2023-11-26 13:53:24 -0300

    instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
     resposta.send("Olá, mundo!")
